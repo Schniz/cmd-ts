8cb58ce280115dd90f33e7cb0411676ea2fcec2f
Gal Schlezinger <gal@spitfire.co.il>
2020-02-23 17:07:25 +0200

    docs

--- src/Parser.ts
+++ src/Parser.ts
@@ -2,6 +2,9 @@ import { ParseItem, ArgParserResult } from './argparse';
 import { Either } from 'fp-ts/lib/Either';
 import { TypeRecord, TRErrors } from './composedType';
 
+/**
+ * A parsing error. Used in `prettyPrint`.
+ */
 export type ParseError<TR extends TypeRecord, CommandConfig = any> = {
   parsed: ArgParserResult;
   errors: TRErrors<TR>;
@@ -19,9 +22,22 @@ export type Into<P extends Parser<any>> = P extends Parser<infer Into>
  * A command line argument parser
  */
 export type Parser<Into = unknown> = {
+  /** Parse strings into values */
   parse(
+    /**
+     * string arguments to parse
+     */
     argv: string[],
+    /**
+     * An optional context.
+     * Provided by the parsing combinators, like `binaryParser` and `subcommands`
+     * to reserve the already-parsed context in the error messages.
+     */
     context?: ParseItem[]
   ): Either<ParseError<TypeRecord>, Into>;
+
+  /**
+   * Description provided for the specific parser
+   */
   description?: string;
 };
