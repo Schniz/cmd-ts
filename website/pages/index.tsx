3c6ec5e878b5ab344e123ca0c43b50654df80dfe
Gal Schlezinger <gal@spitfire.co.il>
2020-03-21 19:00:37 +0200

    Add caching?

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,5 +1,11 @@
+import { Page } from '~/components/Page';
+
 const Home = () => {
-  return <div>Hello!</div>;
+  return (
+    <Page title="cmd-ts">
+      <div>Hello world!</div>
+    </Page>
+  );
 };
 
 export default Home;
