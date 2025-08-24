1f6277a1b33480c79918e6e8a056bcce4fd75109
Gal Schlezinger <gal@spitfire.co.il>
2025-08-24 14:32:39 +0300

    fix: properly reconstruct original argument strings in rest combinator

--- src/rest.ts
+++ src/rest.ts
@@ -1,6 +1,7 @@
 import * as Result from "./Result";
 import type { ArgParser } from "./argparser";
 import type { Descriptive, Displayed, ProvidesHelp } from "./helpdoc";
+import type { AstNode } from "./newparser/parser";
 
 export function rest(
 	config?: Partial<Displayed & Descriptive>,
@@ -22,16 +23,61 @@ export function rest(
 			const visitedNodeIndices = [...context.visitedNodes]
 				.map((x) => context.nodes.indexOf(x))
 				.filter((x) => x > -1);
-			if (visitedNodeIndices.length === 0) {
-				return Result.ok([]);
-			}
 
-			const maxIndex = Math.max(...visitedNodeIndices);
+			const strings: string[] = [];
+
+			const maxIndex = Math.max(-1, ...visitedNodeIndices);
 			const restItems = context.nodes.slice(maxIndex + 1);
 			for (const node of restItems) {
-				context.visitedNodes.add(node);
+				switch (node.type) {
+					case "positionalArgument": {
+						strings.push(node.raw);
+						context.visitedNodes.add(node);
+						break;
+					}
+					case "longOption": {
+						strings.push(...getOriginal(node));
+						context.visitedNodes.add(node);
+						break;
+					}
+					case "shortOption": {
+						strings.push(...getOriginal(node));
+						context.visitedNodes.add(node);
+						break;
+					}
+					case "forcePositional": {
+						strings.push(node.raw);
+						context.visitedNodes.add(node);
+						break;
+					}
+					case "shortOptions": {
+						const last = node.options.at(-1);
+						context.visitedNodes.add(node);
+						strings.push(...getOriginal({ ...node, value: last?.value }));
+						break;
+					}
+				}
 			}
-			return Result.ok(restItems.map((x) => x.raw));
+			return Result.ok(strings);
 		},
 	};
+}
+
+function getOriginal(node: {
+	index: number;
+	raw: string;
+	value?: Extract<AstNode, { type: "longOption" }>["value"];
+}): string[] {
+	if (!node.value) {
+		return [node.raw];
+	}
+
+	if (node.value.delimiter.raw === " ") {
+		return [
+			node.raw.slice(0, node.value.index - node.index),
+			node.value.node.raw,
+		];
+	}
+
+	return [node.raw];
 }
